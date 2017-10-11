9fee42d81eb04ea2f0d03d6ce2299256d32db94c
Vasilis Gkitsas <gkitsasv@gmail.com>
2017-10-11 14:18:30 +0300

    Update TestHTM.m
    Modify SP parameters

--- test/TestHTM.m
+++ test/TestHTM.m
@@ -1,14 +1,12 @@
 [ spHistory] = testSPfun();
-%with testSP call
-%load('cols.mat');
 [numofSPcols,N]=size(spHistory);
 colNum = 512;%original total number of columns in SP        %total number of columns in SP
-cellNum=8;            %cells per column 
+cellNum=4;            %cells per column 
 size1=colNum*cellNum;
-activationThreshold=6;
+activationThreshold=3;
 initialPermanence=0.21;
 connectedPermanence=0.65;
-minThreshold=2;
+minThreshold=1;
 maxNewSynapsesCount=7; 
 totalNum=cellNum*colNum;
 columns=zeros(colNum,1);
@@ -23,21 +21,23 @@ segmentSynapses=zeros(totalNum,totalNum);
 synapsesWeight=zeros(totalNum,totalNum);
 prevSegmentSynapses={}; 
 x=[];y=[];z=[];w=[];prec=[];rec=[];anomaly_score=[];pred_vals=[];act_vals=[];
-ProbArray=[];pred_array=zeros(266,N);
+ProbArray=[];
 learning=1; %enable learning
 activeSynapses=[];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 enableNoise=0;
 %Decoder Paramaters
-ninput= [12*25,3*25,3*25];%256;
+ninput= [10*25,5*25,3*25];%256;
 encParams= struct(...
   'ninput',ninput, ...
-  'aspectRatio_2dtopo',25./18, ...
-  'w',[35,40,35] ...%'buckets',220 ...             % 1-bits w=n-buckets+1
+  'aspectRatio_2dtopo',25./24, ...
+  'w',[10,27,27] ...%'buckets',220 ...             % 1-bits w=n-buckets+1
   );
+bucketNum=ninput(1)-encParams.w(1)+1; %number of buckets for power consumption
+pred_array=zeros(bucketNum,N);
 [ts,N]= test.inputTimeseries();
 encParams= encoder.initPowerDay(encParams,ts);
-decoderAlpha=0.01;prediction_timesteps=2;
+decoderAlpha=0.1;prediction_timesteps=1;
 dec= decoder.SDRClassifier(prediction_timesteps, decoderAlpha, cellNum*colNum, encParams.power.buckets);
 %%%%%%%%%%%%%%%%
 predError=0;
@@ -54,13 +54,13 @@ for stepNum=1:N
     %[~,bucketIndx]=max(pred);
     [~,targetBucket]=max(pred);
     energyConsRange=max(energyMat.reccenterhourly.kw_energy_consumption)-min(energyMat.reccenterhourly.kw_energy_consumption)+10*eps(max(energyMat.reccenterhourly.kw_energy_consumption));
-   %Finding LMS for predictions   
-   x=targetBucket*energyConsRange/380+min(energyMat.reccenterhourly.kw_energy_consumption)-10*eps(max(energyMat.reccenterhourly.kw_energy_consumption));
-   pred_vals(stepNum)=x;act_vals(stepNum)=energyMat.reccenterhourly.kw_energy_consumption(stepNum+120+1);
-   MSError(stepNum)=(energyMat.reccenterhourly.kw_energy_consumption(stepNum+120+1)-x);
+    %Finding LMS for predictions   
+    x=targetBucket*energyConsRange/380+min(energyMat.reccenterhourly.kw_energy_consumption)-10*eps(max(energyMat.reccenterhourly.kw_energy_consumption));
+    pred_vals(stepNum)=x;act_vals(stepNum)=energyMat.reccenterhourly.kw_energy_consumption(stepNum+120+1);
+    MSError(stepNum)=abs((energyMat.reccenterhourly.kw_energy_consumption(stepNum+120+1)-x));
     prevActiveSt=activeSt2;
     if length(sum(predCellsPrev))>0
-    [predError,accuracy,recall,precis]=findAccuracy(activeSt2,reshape(predCellsPrev,1,cellNum*colNum));
+     [predError,accuracy,recall,precis]=findAccuracy(activeSt2,reshape(predCellsPrev,1,cellNum*colNum));
     end
     predCellsPrev=reshape(predictedSt,cellNum,colNum);
     %Store Prediction error
@@ -94,17 +94,20 @@ for stepNum=1:N
         figure(6)
         plot(z,MSError);
         xlabel('Seconds')
-        ylabel('MSE error')
+        ylabel('Error Between Predicted and Raw value')
         figure(7)
         real_values=plot(z,act_vals);
-        set(real_values,'Color','red');
+        set(real_values,'Color','black');
         hold on
-        pred_values=plot(z,pred_vals);
-        set(pred_values,'Color','blue');
+        h=plot(z,pred_vals);
+        set(h,'Color','blue');
+     %   set(h(1),'linewidth','2');
+     %   set(h(2),'linewidth','2');
         xlabel('Seconds')
         ylabel('Energy Consumption value')
         hold off
-        legend('Real Values in timestep t+1','Predicted values for (t+2)')
+        legend('Raw Values in timestep t+1','Predicted values for (t+1)')
    end
 end
+RMSerror=sqrt(sum(MSError.^2)/N)
   %End of Script
